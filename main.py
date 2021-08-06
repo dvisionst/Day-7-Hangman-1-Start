9d6d7d3f24981bee561b56f680beded522ffe928
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-08-06 20:36:43 +0000

    replaced IF block with For loop

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 import random
 chosen_word = word_list[random.randint(0,len(word_list)-1)]
-guess = input("Guess a letter.\n")
-guess = guess.lower()
-print(chosen_word)
+guess = input("Guess a letter:\n").lower()
+#print(chosen_word)
 
-if guess in chosen_word:
-  True
-else:
-  False
+for char in chosen_word:
+  if guess == char:
+    print("Right")
+  else:
+    print("False")
 
 
 
